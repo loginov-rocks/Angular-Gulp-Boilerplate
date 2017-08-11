41e92164ba08ab13208ebd9123f2af18dca31240
Danila Loginov <loginovdanila@gmail.com>
2017-08-11 23:18:15 +0300

    Translations configured

--- src/app/app.config.js
+++ src/app/app.config.js
@@ -4,9 +4,27 @@
   angular.module('app').config(configBlock);
 
   /** @ngInject */
-  function configBlock($locationProvider, $logProvider) {
-    $locationProvider.html5Mode(true);
-    $logProvider.debugEnabled(true);
+  function configBlock($locationProvider, $logProvider, $translateProvider,
+                       tmhDynamicLocaleProvider) {
+    $locationProvider.
+        html5Mode(true);
+
+    $logProvider.
+        debugEnabled(true);
+
+    $translateProvider.
+        useStaticFilesLoader({
+          prefix: '/locales/',
+          suffix: '.json',
+        }).
+        preferredLanguage('en').
+        fallbackLanguage('en').
+        useSanitizeValueStrategy('escape').
+        useMissingTranslationHandlerLog();
+
+    tmhDynamicLocaleProvider.
+        localeLocationPattern('/locales/angular-locale_{{locale}}.js').
+        defaultLocale('en');
   }
 
 })();
