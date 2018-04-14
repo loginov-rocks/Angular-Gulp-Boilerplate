12c5fc37357255f96841c92aa9f7c8e7fa913370
Danila Loginov <danila@loginov.rocks>
2018-04-14 15:56:56 +0300

    feat: rebuild scripts with maps according to relative path

--- gulp/scripts.js
+++ gulp/scripts.js
@@ -24,16 +24,23 @@ gulp.task('scripts:watch', ['scripts'], () => watch());
 /**
  * Build scripts from specified sources.
  * @param {*} src
+ * @param {*} relative
  * @return {*}
  */
-function buildScripts(src) {
+function buildScripts(src, relative = '') {
+  const mapsPath = path.join(
+      path.relative(path.join(config.paths.serve, relative),
+          config.paths.serve),
+      config.paths.maps,
+      relative);
+
   return gulp.src(src).
       pipe($.eslint()).
       pipe($.eslint.format()).
       pipe($.sourcemaps.init()).
       pipe($.babel()).
-      pipe($.sourcemaps.write(config.paths.maps)).
-      pipe(gulp.dest(config.paths.serve));
+      pipe($.sourcemaps.write(mapsPath)).
+      pipe(gulp.dest(path.join(config.paths.serve, relative)));
 }
 
 /**
@@ -52,7 +59,8 @@ function watch(notOnlyChangedCallback) {
           return;
         }
 
-        buildScripts(event.path).
+        buildScripts(event.path,
+            path.relative(config.paths.app, path.dirname(event.path))).
             pipe($.debug({title: 'scripts modified:'})).
             pipe(browserSync.stream());
       });
