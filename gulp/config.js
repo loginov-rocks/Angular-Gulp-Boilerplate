0eef90942f3d5610bd1c113e2a94deef8312fb1e
Danila Loginov <danila@loginov.rocks>
2018-09-30 08:50:34 +0300

    chore: code style

--- gulp/config.js
+++ gulp/config.js
@@ -129,11 +129,11 @@ config.plugins.uglifyAngularLocales = {
  */
 config.plugins.wiredep = {
   directory: 'bower_components',
-  overrides:{
-    "normalize-css":{
-      "main":["normalize.css"]
-    }
-  }
+  overrides: {
+    'normalize-css': {
+      'main': ['normalize.css'],
+    },
+  },
 };
 
 /**
